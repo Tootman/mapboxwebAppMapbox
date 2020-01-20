cd9c9f262f5a2af349cb9e9e3b273d03c5a8b6e4
Dan Simmons <danieljsimmons@btinternet.com>
2020-01-20 10:48:41 +0000

    update to v 0.9.038 fixing map overflow css issue

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ import mapboxgl from "mapbox-gl";
 const state = {};
 state.settings = {};
 state.about = {};
-state.about.version = "0.9.037";
-state.about.releaseDate = "25th Oct 2019";
+state.about.version = "0.9.038";
+state.about.releaseDate = "20th January 2020";
 state.about.content = `<h4> ORCL Client map</h4><p> Occam's Razor Consulting Ltd, parks and open spaces Asset map</p>
 <p> Displaying: <div id="map-name"></div></p>
 <h4> App details </h4>
