201b056e79d2c6883997d02b72f74c1688ab7ecf
Dan Simmons <danieljsimmons@btinternet.com>
2020-05-04 13:13:01 +0100

    udated version to 0.9.049 4May

--- src/main.js
+++ src/main.js
@@ -19,8 +19,8 @@ import mapboxgl from "mapbox-gl";
 const state = {};
 state.settings = {};
 state.about = {};
-state.about.version = "0.9.048";
-state.about.releaseDate = "14th April 2020";
+state.about.version = "0.9.049";
+state.about.releaseDate = "5th May 2020";
 state.about.content = `<h4> ORCL Client map</h4><p> Occam's Razor Consulting Ltd, parks and open spaces Asset map</p>
 <p> Displaying: <div id="map-name"></div></p>
 <h4> App details </h4>
